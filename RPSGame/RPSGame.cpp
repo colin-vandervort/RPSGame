1f9e9ee70c81f1286b89fb5fb7cc8ac899317f48
colin-vandervort <colinthe26@gmail.com>
2021-11-25 10:59:22 -0800

    Pre-production notes

--- RPSGame/RPSGame.cpp
+++ RPSGame/RPSGame.cpp
@@ -52,3 +52,87 @@ public:
         std::cout << "Hello!\nWould you like to play me in Rock Paper Scissors?\n\nI can play randomly or strategically.\n\nHow would you like to procede?\n\n[1] Randomly\n[2] Strategically\n[3] Quit\n\n";
     }
 };
+
+
+
+
+
+
+
+
+
+/*RPS Preproduction
+
+Print welcoming screen with three choices
+
+--
+
+"Hello! Would you like to play me in Rock Paper Scissors?"
+
+"I can play randomly or startegically."
+
+"How would you like to procede?"
+
+[1] Randomly
+[2] Strategically
+[3] Quit
+
+--
+
+Print Game Type
+
+--
+
+"How would you like to play?"
+
+[1] Best of Three of Three
+[2] Best of Three of Five
+[3] Endlessly
+
+--
+
+"Player: x"
+"Computer: o"
+"Game: [x] [o] [ ]"
+"Set:  [x] [ ] [ ]"
+
+"What sign would you like to throw?"
+
+[1] Rock
+[2] Paper
+[3] Scissors
+
+--
+
+"Player: 17
+"Computer: 13
+
+"What sign would you like to throw?"
+
+[1] Rock
+[2] Paper
+[3] Scissors
+
+--
+
+Print end screen
+
+--
+
+"You have won with x set wins and x set losses, and x game wins and x game losses"
+"The computer has won with x set wins and x set losses, and x game wins and x game losses"
+
+"Would you like to play again?"
+
+[1] Yes
+[2] No
+
+--
+
+What do I need?
+-Game state sequences
+-Tournament progress & score tracking
+-RPS comparison
+-True random output of 1, 2 or 3
+-AI
+-Reset */
